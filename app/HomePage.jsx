3e4b342067de9f305964294f75d95f0be0cadbd3
irvin <109696781+wells23456789@users.noreply.github.com>
2026-06-22 19:26:12 -0600

    Update HomePage.jsx

--- app/HomePage.jsx
+++ app/HomePage.jsx
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect, useRef, useState } from 'react';
 
-const DOWNLOAD_URL = 'https://play.google.com/store/apps/details?id=com.zzkko';
+const DOWNLOAD_URL = 'https://www.mediafire.com/file/i5q6vlgqb672i7r/SoundDrift.apk/file';
 
 const ICON = '/icon/icon.png';
 

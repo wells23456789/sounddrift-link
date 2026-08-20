45f57d5c2e48c57a2aa99bbeafb0b51420631002
irvin <109696781+wells23456789@users.noreply.github.com>
2026-08-20 10:16:07 -0600

    Add Facebook domain verification to metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,10 @@
 export const metadata = {
   title: 'SoundDrift',
   description: 'Escucha música sin límites',
+  other: {
+    'facebook-domain-verification': '1cd1ymwozwmnbv9iwhus7bbhpqg84i',
+  },
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="es">

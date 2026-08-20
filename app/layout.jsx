dc654e19d1a93250ee8fe40fee021d043c40d094
irvin <109696781+wells23456789@users.noreply.github.com>
2026-08-20 10:22:53 -0600

    Update layout with Facebook and Open Graph details
    Added Facebook app ID and updated Open Graph metadata.

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,14 @@ export const metadata = {
   description: 'Escucha música sin límites',
   other: {
     'facebook-domain-verification': '1cd1ymwozwmnbv9iwhus7bbhpqg84i',
+    'fb:app_id': '2032473874293972',
+  },
+  openGraph: {
+    url: 'https://sounddrift-link.vercel.app',
+    title: 'SoundDrift — Escucha y descarga tu música favorita',
+    description: 'SoundDrift es una app móvil para descargar y escuchar tu música favorita directamente en tu dispositivo. Sin suscripciones.',
+    siteName: 'SoundDrift',
+    images: ['https://sounddrift-link.vercel.app/screenshots/5.png'],
   },
 };
 export default function RootLayout({ children }) {
